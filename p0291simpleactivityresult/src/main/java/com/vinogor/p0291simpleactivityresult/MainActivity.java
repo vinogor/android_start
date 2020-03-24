4d431f9f5f693f00c4c4b5237f0b6e38ecf8a417
Андреев Александр Сергеевич <ASeAndreev@sberbank.ru>
2020-03-24 14:23:26 +0300

    [P0291_SimpleActivityResult] Extras - Урок 29. Вызываем Activity и получаем результат. Метод startActivityForResult

---
+++ p0291simpleactivityresult/src/main/java/com/vinogor/p0291simpleactivityresult/MainActivity.java
@@ -0,0 +1,42 @@
+package com.vinogor.p0291simpleactivityresult;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    TextView tvName;
+    Button btnName;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        tvName = findViewById(R.id.tvName);
+        btnName = findViewById(R.id.btnName);
+        btnName.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, NameActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) {
+            return;
+        }
+        String name = data.getStringExtra("name");
+        tvName.setText("Your name is " + name);
+    }
+}
