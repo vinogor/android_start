70ab87b41939398e729885ec9159d7070e69ec1f
Андреев Александр Сергеевич <ASeAndreev@sberbank.ru>
2020-03-25 16:53:31 +0300

    [SQLiteTransaction] Урок 38. Транзакции в SQLite. Небольшой FAQ по SQLite

---
+++ p0381sqlitetransaction/src/main/java/com/vinogor/p0381sqlitetransaction/MainActivity.java
@@ -0,0 +1,163 @@
+package com.vinogor.p0381sqlitetransaction;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+    // рекомендуемая форма для использования транзакций такая:
+    /*
+        db.beginTransaction();
+        try {
+            ...
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
+
+     */
+
+    final String LOG_TAG = "myLogs";
+
+    DBHelper dbh;
+    SQLiteDatabase db;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Log.d(LOG_TAG, "--- onCreate Activity ---");
+        dbh = new DBHelper(this);
+        myActions();
+    }
+
+//    void myActions() {
+//        db = dbh.getWritableDatabase();
+//        delete(db, "mytable");
+//        insert(db, "mytable", "val1");
+//        insert(db, "mytable", "val2");
+//        read(db, "mytable");
+//        dbh.close();
+//    }
+
+
+// ======= тут val1 не положится
+//    void myActions() {
+//        db = dbh.getWritableDatabase();
+//        delete(db, "mytable");
+//        db.beginTransaction();
+//        insert(db, "mytable", "val1");
+//        db.endTransaction();
+//        insert(db, "mytable", "val2");
+//        read(db, "mytable");
+//        dbh.close();
+//    }
+
+
+    void myActions() {
+        db = dbh.getWritableDatabase();
+        delete(db, "mytable");
+
+        db.beginTransaction();
+        insert(db, "mytable", "val1");
+        db.setTransactionSuccessful(); // иначе откатится при закрытии транзакции
+        insert(db, "mytable", "val2");
+        db.endTransaction();
+
+        insert(db, "mytable", "val3");
+        read(db, "mytable");
+        dbh.close();
+    }
+
+
+// ====== демонстрация блокировки БД когда транзакция открыта
+//    void myActions() {
+//        try {
+//            db = dbh.getWritableDatabase();
+//            delete(db, "mytable");
+//
+//            db.beginTransaction();
+//            insert(db, "mytable", "val1");
+//
+//            Log.d(LOG_TAG, "create DBHelper");
+//            DBHelper dbh2 = new DBHelper(this);
+//            Log.d(LOG_TAG, "get db");
+//            SQLiteDatabase db2 = dbh2.getWritableDatabase(); // вот тут эксепшон вылетает
+//            read(db2, "mytable");
+//            dbh2.close();
+//
+//            db.setTransactionSuccessful();
+//            db.endTransaction();
+//
+//            read(db, "mytable");
+//            dbh.close();
+//
+//        } catch (Exception ex) {
+//            Log.d(LOG_TAG, ex.getClass() + " error: " + ex.getMessage());
+//        }
+//    }
+
+
+
+//    void myActions() {
+//        db = dbh.getWritableDatabase();
+//        SQLiteDatabase db2 = dbh.getWritableDatabase();
+//        Log.d(LOG_TAG, "db = db2 - " + db.equals(db2));
+//        Log.d(LOG_TAG, "db open - " + db.isOpen() + ", db2 open - " + db2.isOpen());
+//        db2.close();
+//        Log.d(LOG_TAG, "db open - " + db.isOpen() + ", db2 open - " + db2.isOpen());
+//    }
+
+    void insert(SQLiteDatabase db, String table, String value) {
+        Log.d(LOG_TAG, "Insert in table " + table + " value = " + value);
+        ContentValues cv = new ContentValues();
+        cv.put("val", value);
+        db.insert(table, null, cv);
+    }
+
+    void read(SQLiteDatabase db, String table) {
+        Log.d(LOG_TAG, "Read table " + table);
+        Cursor c = db.query(table, null, null, null, null, null, null);
+        if (c != null) {
+            Log.d(LOG_TAG, "Records count = " + c.getCount());
+            if (c.moveToFirst()) {
+                do {
+                    Log.d(LOG_TAG, c.getString(c.getColumnIndex("val")));
+                } while (c.moveToNext());
+            }
+            c.close();
+        }
+    }
+
+    void delete(SQLiteDatabase db, String table) {
+        Log.d(LOG_TAG, "Delete all from table " + table);
+        db.delete(table, null, null);
+    }
+
+    // класс для работы с БД
+    class DBHelper extends SQLiteOpenHelper {
+
+        public DBHelper(Context context) {
+            super(context, "myDB", null, 1);
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(LOG_TAG, "--- onCreate database ---");
+
+            db.execSQL("create table mytable ("
+                    + "id integer primary key autoincrement,"
+                    + "val text"
+                    + ");");
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
+
+}
